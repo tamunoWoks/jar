cd9b33ad574f1508da1c5bff4fe1aefff3404a04
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-08-12 21:30:24 -0600

    Test for valid initialization

--- test_jar.py
+++ test_jar.py
@@ -13,3 +13,8 @@ def test_init():
         Jar(-1)
     with pytest.raises(ValueError):
         Jar(0)
+    
+    # Test valid initialization
+    jar = Jar(12)
+    assert jar.capacity == 12
+    assert jar.size == 0
